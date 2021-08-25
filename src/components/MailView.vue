776edfaa170422b84b63de1260e7517dd56c5db6
hugobytes <20419225+hugobytes@users.noreply.github.com>
2021-08-25 16:07:07 +0100

    Toggle emails

--- src/components/MailView.vue
+++ src/components/MailView.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="bg-white">
+    <div class="flex mb-4">
+      <button class="mr-2 px-3 py-2 rounded bg-blue-500" @click="toggleRead">
+        {{ email.read ? "Mark as Unread (r)" : "Mark as Read (r)" }}
+      </button>
+      <button
+        class="mr-2 px-3 py-2 rounded bg-purple-500"
+        @click="toggleArchived"
+      >
+        {{ email.archived ? "Move to Inbox (e)" : "Archive (e)" }}
+      </button>
+      <button class="mr-2 px-3 py-2 rounded bg-green-500" @click="goOlder">
+        Older
+      </button>
+      <button class="mr-2 px-3 py-2 rounded bg-red-500" @click="goNewer">
+        Newer
+      </button>
+    </div>
     <h2 class="text-xl mt-3">{{ email.subject }}</h2>
     <h3 class="font-semibold mt-2">
       From: {{ email.from }} on
@@ -14,13 +31,46 @@ import marked from "marked";
 import { format } from "date-fns";
 import { defineComponent, PropType } from "vue";
 import Email from "../models/email";
+import axios from "axios";
+import useKeydown from "../composables/use-keydown";
 
 export default defineComponent({
   name: "MailView",
-  setup() {
+  setup(props) {
+    useKeydown([
+      {
+        key: "r",
+        fn: () => toggleRead(),
+      },
+      {
+        key: "e",
+        fn: () => toggleArchived(),
+      },
+    ]);
+
+    const email = props.email;
+
+    const toggleRead = () => {
+      email.read = !email.read;
+      axios.put(`emails/${email.id}`, email);
+    };
+
+    const toggleArchived = () => {
+      email.archived = !email.archived;
+      axios.put(`emails/${email.id}`, email);
+    };
+
+    const goNewer = () => {};
+
+    const goOlder = () => {};
+
     return {
       marked,
       format,
+      toggleRead,
+      toggleArchived,
+      goNewer,
+      goOlder,
     };
   },
   props: {
