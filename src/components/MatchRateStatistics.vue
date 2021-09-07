d111ce8b738e45b5ca60a5edf2f5bf77987adf2b
hugobytes <20419225+hugobytes@users.noreply.github.com>
2021-09-07 11:00:05 +0100

    ....

--- src/components/MatchRateStatistics.vue
+++ src/components/MatchRateStatistics.vue
@@ -1,24 +1,66 @@
 <template>
-  <div
-    v-for="[key, value] in stats"
-    class="flex items-cente text-lg"
-    :key="key"
-  >
-    <div class="font-bold mr-2">{{ key }}:</div>
-    <div class="mr-2">{{ value }}</div>
+  <h1 class="text-8xl font-bold">{{ average_match_rate }}</h1>
+  <h2 class="text-xl font-semibold mt-1">Average match rate</h2>
+
+  <div class="flex">
+    <div class="flex-1">
+      <h1 class="text-6xl font-bold mt-12">
+        {{ average_match_rate_per_application }}
+      </h1>
+      <h2 class="text-xl font-semibold mt-1">
+        Average match rate per application
+      </h2>
+      <div
+        v-for="[key, value] in average_match_rate_per_application_per_sector"
+        class="ml-2 flex items-cente text-sm"
+        :key="key"
+      >
+        <div class="font-bold mr-2">{{ key }}:</div>
+        <div class="mr-2">{{ value }}</div>
+      </div>
+    </div>
+
+    <div class="flex-1">
+      <h1 class="text-6xl font-bold mt-12">
+        {{ average_match_rate_per_shortlisted }}
+      </h1>
+      <h2 class="text-xl font-semibold mt-1">
+        Average match rate per shortlisted candidate
+      </h2>
+      <div
+        v-for="[key, value] in average_match_rate_per_shortlisted_per_sector"
+        class="ml-2 flex items-cente text-sm"
+        :key="key"
+      >
+        <div class="font-bold mr-2">{{ key }}:</div>
+        <div class="mr-2">{{ value }}</div>
+      </div>
+    </div>
   </div>
+  <div class="pb-24" />
 </template>
 
 <script>
 import axios from "axios";
 export default {
   async setup() {
-    const { data } = await axios.get("match-rate-statistics");
+    const { data: stats } = await axios.get("match-rate-statistics");
 
-    console.log(new Map(Object.entries(data)));
+    const average_match_rate_per_application_per_sector = new Map(
+      Object.entries(stats.average_match_rate_per_application_per_sector)
+    );
+    const average_match_rate_per_shortlisted_per_sector = new Map(
+      Object.entries(stats.average_match_rate_per_shortlisted_per_sector)
+    );
 
     return {
-      stats: new Map(Object.entries(data)),
+      average_match_rate: stats.average_match_rate,
+      average_match_rate_per_application:
+        stats.average_match_rate_per_application,
+      average_match_rate_per_application_per_sector,
+      average_match_rate_per_shortlisted:
+        stats.average_match_rate_per_shortlisted,
+      average_match_rate_per_shortlisted_per_sector,
     };
   },
 };
